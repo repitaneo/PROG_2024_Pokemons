f85b74d2d6aa58d4a87be0cc470997786febe662
Marcos <Marcos@Profesor>
2024-03-15 08:47:38 +0100

    clase Pokemon terminada
    - setters y getter de edad y nombre
    - toString()
    - Pokemon(int, String)

--- src/pokemons/Pokemon.java
+++ src/pokemons/Pokemon.java
@@ -1,5 +1,48 @@
 package pokemons;
 
-public class Pokemon {
+public abstract class Pokemon {
 
+	private int edad;
+	private String nombre;
+	
+	
+	public Pokemon(int edad,String nombre) {
+		
+		this.nombre = nombre;
+		setEdad(edad);
+	}
+	
+	
+	
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		
+		if(edad<=0) {
+			this.edad = 1;
+		}
+		else this.edad = edad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	
+	@Override
+	public String toString() {
+		return "Pokemon [(" + edad + ") " + nombre + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
 }
