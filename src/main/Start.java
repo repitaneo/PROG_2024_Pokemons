368bf83ffbd6c0e759c6563442d06fbc722015d1
Marcos <Marcos@Profesor>
2024-03-15 09:24:42 +0100

    Start con cosas
    Se crean tres pokemons y se imprimen y consulta su evolución

--- src/main/Start.java
+++ src/main/Start.java
@@ -1,10 +1,33 @@
 package main;
 
+import pokemons.*;
+
+
+
 public class Start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Pikachu pika = new Pikachu(12,"PikaPika",12323);
+		Charmander charman = new Charmander(23,"Charman",34);
+		Squirtle squirtle = new Squirtle(31,"Tortuguita",77);
+
+		Pokemon pokemons[] = new Pokemon[3];
+		pokemons[0] = pika;
+		pokemons[1] = charman;
+		pokemons[2] = squirtle;
+
+		for (int i=0;i<pokemons.length;i++) {
+			
+			System.out.println(i);
+			System.out.println(pokemons[i]);
+			System.out.println(pokemons[i].evolucion());
+		}
+		
+		
+		
+		
+		
 	}
 
 }
