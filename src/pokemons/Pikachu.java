a820e2cf6c00afda130eb7fa09f564d12b4f91cc
Marcos <Marcos@Profesor>
2024-03-15 09:00:04 +0100

    Clase Pikachu
    - getters y setters de velocidad
    - implentación de la operación public String evolucion()
    - toString()

---
+++ src/pokemons/Pikachu.java
@@ -0,0 +1,49 @@
+package pokemons;
+
+public class Pikachu extends Pokemon {
+
+	private int velocidad;
+
+	
+	public Pikachu(int edad,String nombre,int velocidad) {
+		
+		super(edad,nombre);
+		setVelocidad(velocidad);
+	}
+	
+	
+	public int getVelocidad() {
+		return velocidad;
+	}
+	public void setVelocidad(int velocidad) {
+		
+		if(velocidad<100) {
+			this.velocidad = 100;
+		}
+		else this.velocidad = velocidad;
+	}
+
+
+	@Override
+	public String evolucion() {
+		return "Raichu";
+	}
+
+
+	@Override
+	public String toString() {
+		return "Pikachu [{" + velocidad + "} (" + getEdad() + ") " + getNombre() + "]";
+	}
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
